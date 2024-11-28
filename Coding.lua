21145830cf66d386f61f5ebaf20fbd2531ea5a45
E1Company <167244059+E1Company@users.noreply.github.com>
2024-11-27 20:11:24 -0600

    Horror Melon Mod
    A Mod For Melon Sandbox

---
+++ Coding.lua
@@ -0,0 +1,16 @@
+-- Update to use your new textures
+entities.horror_melon = {
+    name = "Horror Melon",
+    textures = {
+        feet = "feets.png",
+        torso = "body.png",
+        head = "head.png",
+        arms = "Textures.png",
+        legs = "Textures.png"
+    },
+    hp = 100,
+    damage = 20,
+    speed = 1.5,
+    
+    -- Rest of your entity definition...
+}
